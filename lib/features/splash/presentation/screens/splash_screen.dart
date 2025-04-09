22408a07cb61a40d7920a2dd6d81a2eea44bea42
Assyl <assylzfg@gmail.com>
2025-04-09 10:32:18 +0500

    Do image in the splash scree

---
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 30, 24, 47),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset("assets/Group 3090.png"),
+              Text(
+                "Dicover all about sport",
+                style: GoogleFonts.sourceSans3(fontSize: 50, ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
